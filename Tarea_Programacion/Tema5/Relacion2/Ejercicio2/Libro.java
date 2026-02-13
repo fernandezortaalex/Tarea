cb0d3542e1d9ae2cfddf620f73ed4d59ecd9e89a
fernandezortaalex <fernandezortaalex001.alu@stemgranada.com>
2026-02-13 14:12:32 +0100

    ejercicios

---
+++ Tarea_Programacion/Tema5/Relacion2/Ejercicio2/Libro.java
@@ -0,0 +1,12 @@
+public class Libro {
+    //Atributos
+    private int id;
+    private String titulo;
+    private String autor;
+    private String sinopsis;
+    private boolean disponible;
+
+    public Libro(int id, String titulo, String autor, String sinopsis, boolean disponible){
+        
+    }
+}
