df47e1aa7105e2da37da2377ddc2811f80b091f4
fernandezortaalex <fernandezortaalex001.alu@stemgranada.com>
2026-02-15 19:21:37 +0100

    programacion

--- Tarea_Programacion/Tema5/Relacion2/Ejercicio2/Libro.java
+++ Tarea_Programacion/Tema5/Relacion2/Ejercicio2/Libro.java
@@ -16,4 +16,54 @@ public class Libro {
         this.sinopsis = sinopsis;
         this.disponible = disponible;
     }
+
+    /**
+     * Muestro el valor del id
+     * @return
+     */
+    public int getId(){
+        return id;
+    }
+
+    /**
+     * Si es mayor o igual a 0 guardo el valor del id
+     * @param id
+     */
+    public void setId(int id){
+        if (id >= 0) {
+            this.id = id;
+        }
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public String getAutor(){
+        return autor;
+    }
+
+    public void getAutor(String autor){
+        this.autor = autor;
+    }
+
+    public String getSinopsis(){
+        return sinopsis;
+    }
+
+    public void getSinopsis(String sinopsis){
+        this.sinopsis = sinopsis;
+    }
+
+    public Boolean getDisponible(){
+        return disponible;
+    }
+
+    public void getDisponible(boolean disponible){
+        this.disponible = disponible;
+    }
 }
