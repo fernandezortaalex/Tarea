a0aa5650e8b1e223507d4638c4d6722fcec5c833
fernandezortaalex <fernandezortaalex001.alu@stemgranada.com>
2026-02-28 16:52:31 +0100

    programacion

--- Tarea_Programacion/Tema5/Ejercicios/src/EjercicioExamen/Examen.java
+++ Tarea_Programacion/Tema5/Ejercicios/src/EjercicioExamen/Examen.java
@@ -65,51 +65,133 @@ public class Examen {
      */
     public static void aniadirAnimales(int numeroAnimalesAniadir, ArrayList<Animal>ArrayAnimalesAniadir){
         int contadorAnimales = 0;
-        while (contadorAnimales <= numeroAnimalesAniadir) {
-            Animal animal = new Animal(Faker.nombreAnimal(), Faker.cantidadAnimal(), Faker.precioAnimal());
-            contadorAnimales+=animal.getCantidad();
+        String nombreAnimales;
+        int cantidadAnimales;
+        double precioAnimales;
+        while (contadorAnimales < numeroAnimalesAniadir) {
+            nombreAnimales = Faker.nombreAnimal();
+            precioAnimales = Faker.precioAnimal();
+            do {
+                cantidadAnimales = Faker.cantidadAnimal();
+            } while (cantidadAnimales == 0);
+
+            if (contadorAnimales + cantidadAnimales > numeroAnimalesAniadir) {
+                cantidadAnimales = numeroAnimalesAniadir - contadorAnimales;
+            }
+            contadorAnimales+=cantidadAnimales;
+            Animal animal = new Animal(nombreAnimales, cantidadAnimales, precioAnimales);  
             ArrayAnimalesAniadir.add(animal);
             
         }
     }
 
+    public static int validarQueSeaNumero(Scanner sc, String mensaje) {
+        int numero = 0;
+        boolean valido = false;
+        do {
+            try {
+                mostrarMensaje(mensaje);
+                numero = Integer.parseInt(sc.nextLine());
+                if (numero > 0) {
+                    valido = true;
+                }else{
+                    mostrarMensaje("Introduce un numero valido");
+                }
+                
+            } catch (NumberFormatException e) {
+                mostrarMensaje("Introduce un numero");
+            }
+            
+        } while (!valido);
+        return numero;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        //Creo el primer animal
-        // Animal animal1 = new Animal("Perrp", 24, 34543);
-        // Animal animal2 = new Animal("Perrp", 24, 34543);
         final int NUMEROANIMALES = 1000;
         ArrayList<Animal> animalesEnZoo = new ArrayList<>();
         aniadirAnimales(NUMEROANIMALES, animalesEnZoo);
 
-
-        //Creo un array de animales donde van a ir todos los animales
-        // animales.add(animal1);
-        // animales.add(animal2);
-
-        //Creo el nuevo zoologico y le paso el array con todos los animales introducidos
         Zoologico zoologico1 = new Zoologico(animalesEnZoo);
 
         int opcion = 0;
-        int posicionAnimales = 0;
+        boolean booleanoAnimales;
+        String codigoAnimal;
+        int cantidadAnimales;
+        
         do {
             opcion = mostrarMenuYLeerOpcion(scanner, 1, 6, "Que opcion quieres elegir:\n1. Mostrar todos los animales.\n2. Buscar un animal por código\n3. Agregar ejemplares a un animal.\n4. Retirar ejemplares de un animal. \n5. Eliminar un animal\n6.Salir.");
             if (opcion == 1) {
                 mostrarMensaje(zoologico1.mostrarAnimales());
             }else if (opcion == 2) {
                 /**
-                 * LLamo al metodo {posicionAnimales} y le paso el codigo del animal, si es -1 imprime error
-                 * en caso contrario imprime el animal de la posicion devuelta
+                 * Guardo el resultado del detodo buscarAnimalCodigo y si devulve null sale un mensaje de error
+                 * si no devuelve al animal
                  */
-                posicionAnimales = zoologico1.posicionAnimal(leerStringNoVacio("Codigo del animal a buscar", scanner));
-                if (posicionAnimales != -1) {
-                    mostrarMensaje("El animal es " + animalesEnZoo.get(posicionAnimales));
+                Animal encontrado = zoologico1.buscarAnimalCodigo(leerStringNoVacio("Codigo del animal a buscar", scanner));
+                if (encontrado != null) {
+                    mostrarMensaje(encontrado.toString());
                 }else{
                     mostrarMensaje("Animal no encontrado");
                 }
-            }else if (opcion == 3) {
                 
+            }else if (opcion == 3) {
+                /**
+                 * Llamo a la funcion {@link Examen#leerStringNoVacio}
+                 */
+                codigoAnimal = leerStringNoVacio("Codigo del animal a añadir ejemplares", scanner);
+                /**
+                 * Llamo a la funcion {@link Examen#validarQueSeaNumero}
+                 */
+                cantidadAnimales = validarQueSeaNumero(scanner, "Cantidad de animales a añadir");
+                /**
+                 * Llamo a la funcion {@link zoologico1#agregarEjemplares}
+                 */
+                booleanoAnimales = zoologico1.agregarEjemplares(codigoAnimal, cantidadAnimales);
+                /**
+                 * Segun el resultado del metodo imprimo un mensaje
+                 */
+                if (!booleanoAnimales) {
+                    mostrarMensaje("No se ha podido añadir los animales");
+                }else{
+                    mostrarMensaje("Se han añadido los animales");
+                }
+            }else if (opcion == 4) {
+                /**
+                 * Llamo a la funcion {@link Examen#leerStringNoVacio}
+                 */
+                codigoAnimal = leerStringNoVacio("Codigo del animal a retirar ejemplares", scanner);
+                /**
+                 * Llamo a la funcion {@link Examen#validarQueSeaNumero}
+                 */
+                cantidadAnimales = validarQueSeaNumero(scanner, "Cantidad de animales a retirar");
+                /**
+                 * Creo un arrayList para control de errores llamando a {@link zoologico1#retirarEjemplares}
+                 */
+                ArrayList<Boolean> retirarAnimalesZoo = zoologico1.retirarEjemplares(codigoAnimal, cantidadAnimales);
+                if (retirarAnimalesZoo.get(0)) {
+                    mostrarMensaje("Animales retirados con exito");
+                }else if (!retirarAnimalesZoo.get(1)) {
+                    mostrarMensaje("Animal no encontrado.");
+                }else if (!retirarAnimalesZoo.get(2)) {
+                    mostrarMensaje("No se puede retirar más ejemplares de los disponibles.");
+                }
+            }else if (opcion == 5) {
+                /**
+                 * Llamo a la funcion {@link Examen#leerStringNoVacio}
+                 */
+                codigoAnimal = leerStringNoVacio("Codigo del animal a eliminar", scanner);
+                /**
+                 * Creo un arrayList para control de errores llamando a {@link zoologico1#eliminarAnimal}
+                 */
+                ArrayList<Boolean> retirarAnimalesZoo = zoologico1.eliminarAnimal(codigoAnimal);
+                if (retirarAnimalesZoo.get(0)) {
+                    mostrarMensaje("Animal eliminado con exito");
+                }else if (!retirarAnimalesZoo.get(1)) {
+                    mostrarMensaje("Animal no encontrado.");
+                }else if (!retirarAnimalesZoo.get(2)) {
+                    mostrarMensaje("No se puede eliminar un animal con ejemplares disponibles.");
+                }
             }
         } while (opcion != 6);
         scanner.close();
--- Tarea_Programacion/Tema5/Ejercicios/src/EjercicioExamen/Zoologico.java
+++ Tarea_Programacion/Tema5/Ejercicios/src/EjercicioExamen/Zoologico.java
@@ -1,21 +1,23 @@
 package Ejercicios.src.EjercicioExamen;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Zoologico {
     private ArrayList<Animal> animales;
 
     /**
      * Constructor
+     * 
      * @param animales
      */
     public Zoologico(ArrayList<Animal> animales) {
         setAnimales(animales);
     }
 
-
     /**
      * Devuelvo el animal
+     * 
      * @return
      */
     public ArrayList<Animal> getAnimales() {
@@ -23,18 +25,19 @@ public class Zoologico {
     }
 
     public void setAnimales(ArrayList<Animal> animales) {
-        
+
         this.animales = animales;
-        
+
     }
-    
+
     /**
      * Muestra todos los animales que hay en el array de animales
+     * 
      * @param separador Elemento que hace de separador
      * @return Devuelve un string de informacion del animal
      */
-    public String mostrarAnimales(){
-        String mensaje= "";
+    public String mostrarAnimales() {
+        String mensaje = "";
         for (Animal animal : animales) {
             mensaje += animal;
         }
@@ -42,41 +45,87 @@ public class Zoologico {
     }
 
     /**
-     * Le paso un codigo y va recorriendo el array de animales hasta que el codigo introducido
-     * coincida con un codigo de los animales. Si coincide devuelve la posicion de i,
-     * si no le devuelve un numero negativo. 
-     * @param codigo Codigo del animal a buscar
-     * @return posicion de ese animal
+     * Le paso un codigo y va recorriendo el array de animales hasta que el codigo
+     * introducido
+     * coincida con un codigo de los animales. Si coincide devuelve el animal
+     * si no le devuelve un numero negativo.
+     * @param codigo
+     * @return
      */
-    public int posicionAnimal(String codigo){
+    public Animal buscarAnimalCodigo(String codigo){
         boolean encontrado = false;
-        int posicionAnimal = -1;
+        Animal animalEncontrado = null;
         for (int i = 0; i < animales.size() && encontrado != true; i++) {
             if (animales.get(i).getCodigo().equalsIgnoreCase(codigo)) {
-                posicionAnimal = i;
+                animalEncontrado = animales.get(i);
                 encontrado = true;
             }
         }
-        return posicionAnimal;
+        return animalEncontrado;
     }
 
     /**
      * Corregirlo para llamar a la funcion {@link Zoologico#posicionAnimal(String)}
      * y con el numero que te devuelva añadir la cantidad que le indiques
+     * 
      * @param codigo
      * @param cantidadAniadir
      */
-    public void agregarEjemplares(String codigo, int cantidadAniadir){
+    public boolean agregarEjemplares(String codigo, int cantidadAniadir) {
         boolean encontrado = false;
-        for (int i = 0; i < animales.size() && encontrado != true; i++) {
-            if (animales.get(i).getCodigo().equalsIgnoreCase(codigo)) {
-                animales.get(i).setCantidad(animales.get(i).getCantidad() + cantidadAniadir);
-                encontrado = true;
+        Animal animal = buscarAnimalCodigo(codigo);
+        if (animal != null) {
+            animal.setCantidad(animal.getCantidad() + cantidadAniadir) ;
+            encontrado = true;
+        }
+        return encontrado;
+    }
+
+    /**
+     * Voy haciendo if else con cada condicion, por defecto el arrayList de booleanosesta en negativo
+     * si una condicion no se cumple su posicion se queda en false, asi cuando vaya al main 
+     * si esa posicion esta en false es ahi donde esta el error.
+     * 1= animal no existe
+     * 2= retirar mas cantidad de la que hay de animales
+     * @param codigo
+     * @param cantidadRetirar
+     * @return
+     */
+    public ArrayList<Boolean> retirarEjemplares(String codigo, int cantidadRetirar){
+        ArrayList<Boolean> controlErrores = new ArrayList<>(Arrays.asList(false, false, false));
+        Animal animal = buscarAnimalCodigo(codigo);
+        if (animal != null) {
+            controlErrores.set(1, true);
+            if (animal.getCantidad() >= cantidadRetirar) {
+                controlErrores.set(2, true);
+                controlErrores.set(0, true);
+                animal.setCantidad(animal.getCantidad() - cantidadRetirar) ;
+                
             }
         }
-        if (animalAMostrar.isEmpty()) {
-            animalAMostrar = "Animal no encontrado.";
-        }
+        return controlErrores;
     }
+
+    /**
+     * 1= animal no existe
+     * 2= retirar animal con cantidad mayor a 0
+     * @param codigo
+     * @return
+     */
+    public ArrayList<Boolean> eliminarAnimal(String codigo){
+        ArrayList<Boolean> controlErrores = new ArrayList<>(Arrays.asList(false, false, false));
+        Animal animal = buscarAnimalCodigo(codigo);
+        if (animal != null) {
+            controlErrores.set(1, true);
+            if (animal.getCantidad() == 0) {
+                controlErrores.set(2, true);
+                controlErrores.set(0, true);
+                animales.remove(animal);
+            }
+        }
+        return controlErrores;
+    }
+
+
 
 }
