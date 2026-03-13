9ffbad2cb1b9572db5931637054221e4ae3a52d3
fernandezortaalex <fernandezortaalex001.alu@stemgranada.com>
2026-03-13 09:36:22 +0100

    gtdbfb

---
+++ Tarea_Programacion/Tema6/EjerciciosT6/src/Relacion1/Ejercicio7/Autobus.java
@@ -0,0 +1,26 @@
+package EjerciciosT6.src.Relacion1.Ejercicio7;
+
+public class Autobus implements Transporte {
+    String horaParada;
+    String horaSalida;
+    
+    public Autobus(String horaParada, String horaSalida) {
+        this.horaParada = horaParada;
+        this.horaSalida = horaSalida;
+    }
+
+    public String arrancar(){
+        return "El bus arranco";
+    }
+    public String detener(){
+        return "El bus se detuvo";
+    }
+
+    @Override
+    public String toString() {
+        return "Autobus [horaParada=" + horaParada + ", horaSalida=" + horaSalida + ", arrancar()=" + arrancar()
+                + ", detener()=" + detener() + "]";
+    }
+
+    
+}
---
+++ Tarea_Programacion/Tema6/EjerciciosT6/src/Relacion1/Ejercicio7/Transporte.java
@@ -0,0 +1,6 @@
+package EjerciciosT6.src.Relacion1.Ejercicio7;
+
+interface Transporte {
+    String arrancar();
+    String detener();
+}
