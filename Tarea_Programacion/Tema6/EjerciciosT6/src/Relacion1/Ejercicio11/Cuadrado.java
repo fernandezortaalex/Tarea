fb10fe8bd7c0863d95a2f0644ace1cd7b57ec855
fernandezortaalex <fernandezortaalex001.alu@stemgranada.com>
2026-03-26 18:54:18 +0100

    sgsrg

---
+++ Tarea_Programacion/Tema6/EjerciciosT6/src/Relacion1/Ejercicio11/Cuadrado.java
@@ -0,0 +1,16 @@
+package EjerciciosT6.src.Relacion1.Ejercicio11;
+
+public class Cuadrado extends Figura {
+    private double lado;
+    
+    public Cuadrado(double lado) {
+        this.lado = lado;
+    }
+
+
+
+    @Override
+    double calcularArea() {
+        return lado * lado;
+    }
+}
---
+++ Tarea_Programacion/Tema6/EjerciciosT6/src/Relacion1/Ejercicio14/Producto.java
@@ -0,0 +1,32 @@
+package EjerciciosT6.src.Relacion1.Ejercicio14;
+
+public abstract class Producto {
+    private String nombre;
+    private double precio;
+    
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public abstract String toString();
+    
+    
+}
